8e757e78c4b424fd984d7af221f1e59e37389362
Philip Costache <nogobaduk@gmail.com>
2022-05-24 14:24:01 -0400

    understanding the basics: sending responses

--- 01_Understanding_the_Basics/index.js
+++ 01_Understanding_the_Basics/index.js
@@ -1,7 +1,15 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log(req)
+  // console.log(req)
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>My First Page</title></head>');
+  res.write('<body><h1>Hello</h1></body>')
+  res.write('</html>');
+  res.end();
+
+  // process.exit();
 });
 
 server.listen(3000);
