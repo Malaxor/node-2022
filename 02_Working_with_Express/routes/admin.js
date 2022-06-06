9113807def61d107fc6b17dfebd94136a14df664
Philip Costache <nogobaduk@gmail.com>
2022-06-06 19:25:08 -0400

    working with express: filtering paths

--- 02_Working_with_Express/routes/admin.js
+++ 02_Working_with_Express/routes/admin.js
@@ -2,14 +2,14 @@ const router = require('express').Router();
 
 router.get('/add-product', (req, res) => {
   res.send(
-    `<form action='/product' method='POST'>
+    `<form action='/admin/add-product' method='POST'>
       <input type='text' name='title'>
       <button type='submit'>Submit</button>
     </form>`
   );
 });
 
-router.post('/product', (req, res) => {
+router.post('/add-product', (req, res) => {
   console.log(req.body);
   res.redirect('/');
 });
