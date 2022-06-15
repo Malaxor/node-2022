be4b3d9496915167408648b8b8673c677ba7db33
Philip Costache <nogobaduk@gmail.com>
2022-06-15 19:14:12 -0400

    enhancing the app: editing and deleting products

--- 04_MVC/routes/admin.js
+++ 04_MVC/routes/admin.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
-const { getAddProduct, postAddProduct } = require('../controllers/admin');
+const { getAddProduct, postAddProduct, getProducts } = require('../controllers/admin');
 
 router.route('/add-product').get(getAddProduct).post(postAddProduct);
+router.route('/products').get(getProducts)
 
 module.exports = router;
