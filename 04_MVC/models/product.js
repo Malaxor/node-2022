2690bf3860db0c93dfc1dbfce262e4f634a839cf
Philip Costache <nogobaduk@gmail.com>
2022-06-14 23:44:25 -0400

    enhancing the app: creating shop structure and adding nav links

--- 04_MVC/models/product.js
+++ 04_MVC/models/product.js
@@ -2,17 +2,21 @@ const fs = require('fs');
 const path = require('path');
 
 const p = path.join(path.dirname(require.main.filename), 'data', 'products.json');
+const getFileContent = (cb) => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      return cb([]);
+    }
+    cb(JSON.parse(fileContent));
+  });
+}
 
 module.exports = class Product {
   constructor(title) {
     this.title = title;
   }
   save() {
-    fs.readFile(p, (err, fileContent) => {
-      let products = [];
-      if (!err) {
-        products = JSON.parse(fileContent);
-      }
+    getFileContent(products => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), (err) => {
         console.log(err);
@@ -20,11 +24,6 @@ module.exports = class Product {
     });
   }
   static fetchAll(cb) {
-    fs.readFile(p, (err, fileContent) => {
-      if (err) {
-        cb([]);
-      }
-      cb(JSON.parse(fileContent));
-    });
+    getFileContent(cb);
   }
 }
